1f3632b07c9b641cabdc6e465820351db774792b
sahat17 <144116059+sahat17@users.noreply.github.com>
2024-05-23 10:39:46 +0700

    Add files via upload

---
+++ monitrol_minitoring_dht_dan_kontrol_led_firebase_mit_inventor.ino
@@ -0,0 +1,94 @@
+// Library yang dibutuhkan
+#include "DHT.h"
+#include <FirebaseESP8266.h>
+#include <ESP8266WiFi.h>
+
+// Mendefinisikan pin dan tipe sensor DHT
+#define DHTPIN D6
+#define DHTTYPE DHT11
+DHT dht11(DHTPIN, DHTTYPE);
+
+int led = D7;
+
+// Isikan sesuai pada Firebase
+#define FIREBASE_HOST "https://monitrol-dht-led-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "9kXvV3wQfLAvGmDGQGKbeeJc06P5fHe0tAd8ZhKe"
+
+//Nama Wifi
+#define WIFI_SSID "DENAME"
+#define WIFI_PASSWORD "10101010"
+
+// mendeklarasikan objek data dari FirebaseESP8266
+FirebaseData firebaseData;
+
+void setup() {
+  Serial.begin(115200);
+
+  dht11.begin();
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW); //nilai awal relay akan off
+  // Koneksi ke Wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+
+
+void loop() {
+  // Sensor DHT11 membaca suhu dan kelembaban
+  float t = dht11.readTemperature();
+  float h = dht11.readHumidity();
+
+  // Memeriksa apakah sensor berhasil mambaca suhu dan kelembaban
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Gagal membaca sensor DHT11");
+    delay(1000);
+    return;
+  }
+
+  // Menampilkan suhu dan kelembaban kepada serial monitor
+  Serial.print("Suhu: ");
+  Serial.print(t);
+  Serial.println(" *C");
+  Serial.print("Kelembaban: ");
+  Serial.print(h);
+  Serial.println(" %");
+  Serial.println();
+
+  // Memberikan status suhu dan kelembaban kepada firebase
+  if (Firebase.setFloat(firebaseData, "/Data/suhu", t)) {
+    Serial.println("Suhu terkirim");
+  } else {
+    Serial.println("Suhu tidak terkirim");
+    Serial.println("Karena: " + firebaseData.errorReason());
+  }
+
+  if (Firebase.setFloat(firebaseData, "/Data/kelembaban", h)) {
+    Serial.println("Kelembaban terkirim");
+    Serial.println();
+  } else {
+    Serial.println("Kelembaban tidak terkirim");
+    Serial.println("Karena: " + firebaseData.errorReason());
+  }
+
+  if (Firebase.getString(firebaseData, "/Data/ledValue")) {
+    String ledValueStr = firebaseData.stringData();
+    if (ledValueStr == "1") {
+      digitalWrite(led, HIGH);
+    } else if (ledValueStr == "0") {
+      digitalWrite(led, LOW);
+    }
+  }
+
+  delay(1000);
+}
